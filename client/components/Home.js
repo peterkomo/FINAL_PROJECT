c22fad331264c9d1ff95f1219e2fd0b2c6af38bf
peterkomo <mwangipeterkomo@gmail.com>
2023-10-25 19:18:31 +0300

    trial one

---
+++ client/components/Home.js
@@ -0,0 +1,121 @@
+import React from "react";
+import "/Home.css";
+
+export const Homepage = () => {
+  return (
+    <div className="homepage">
+      <div className="div">
+        <div className="overlap">
+          <div className="overlap-group">
+            <div className="text">{""}</div>
+            <img className="learn" alt="Learn" src="learn-1.png" />
+            <img className="image" alt="Image" src="image-1.png" />
+            <div className="text-wrapper">Nurturing Africa’s Tech Talent</div>
+            <p className="p">
+              Through market-aligned skills training, we help learners build new career possibilities with greater
+              confidence and capability.
+            </p>
+          </div>
+          <img className="NOTIFICATON" alt="Notificaton" src="NOTIFICATON.png" />
+          <div className="ellipse" />
+        </div>
+        <div className="overlap-group-2">
+          <div className="rectangle" />
+          <img className="IG" alt="Ig" src="IG.png" />
+          <img className="TWEETER" alt="Tweeter" src="TWEETER.png" />
+          <img className="EML" alt="Eml" src="EML.png" />
+          <div className="text-wrapper-2">@MORINGADESK</div>
+          <div className="text-wrapper-3">@moringadeskofficial</div>
+          <div className="text-wrapper-4">@moringadesl@gmail.com</div>
+          <img className="phone" alt="Phone" src="phone.png" />
+          <div className="text-wrapper-5">contact us on: 0778456342</div>
+          <div className="text-wrapper-6">@copyrightreservedformoringadesk</div>
+        </div>
+        <p className="discover-grow">
+          <span className="span">Discover.</span>
+          <span className="text-wrapper-7">&nbsp;</span>
+          <span className="text-wrapper-8">Grow.</span>
+          <span className="text-wrapper-7">&nbsp;</span>
+          <span className="text-wrapper-9">Transform.</span>
+        </p>
+        <p className="text-wrapper-10">
+          In Moringa we are transforming the way tech education is done in African Markets. All our programs are
+          delivered through a blended learning approach that combines market-aligned courses, a classroom team of
+          talented mentors with the skills and knowledge to deliver growth and results for learners, and an environment
+          that supports student creativity, job market preparation in a fun, open, and transformative learning
+          experience. We understand that the learning experience may not always be smooth. Bearing that in mind, this
+          support platform enables our students to easily access help and/or solutions in times of need.
+        </p>
+        <img className="img" alt="Rectangle" src="rectangle-45.png" />
+        <img className="rectangle-2" alt="Rectangle" src="rectangle-46.png" />
+        <img className="rectangle-3" alt="Rectangle" src="rectangle-48.png" />
+        <img className="rectangle-4" alt="Rectangle" src="rectangle-47.png" />
+        <div className="flexcontainer">
+          <p className="span-wrapper">
+            <span className="span">
+              Reach out.
+              <br />
+            </span>
+          </p>
+          <p className="span-wrapper">
+            <span className="text-wrapper-11">
+              Post any inquiries to the support forum and get responses from other students or Technical Mentors.
+            </span>
+          </p>
+        </div>
+        <div className="flexcontainer-2">
+          <p className="text-2">
+            <span className="span">
+              Search.
+              <br />
+            </span>
+          </p>
+          <p className="text-2">
+            <span className="text-wrapper-12">
+              Describe your problem using key words to check for similar problems amd find a solution quicker.
+            </span>
+          </p>
+        </div>
+        <div className="flexcontainer-3">
+          <p className="text-2">
+            <span className="span">Respond</span>
+            <span className="text-wrapper-7">
+              .<br />
+            </span>
+          </p>
+          <p className="text-2">
+            <span className="text-wrapper-11">
+              Help resolve other students queries by suggesting solutions to their posted problem.
+            </span>
+          </p>
+        </div>
+        <div className="flexcontainer-4">
+          <p className="span-wrapper">
+            <span className="span">
+              Filter.
+              <br />
+            </span>
+          </p>
+          <p className="span-wrapper">
+            <span className="text-wrapper-11">
+              Look through tags to find discussions in line with where your problem may lie.
+            </span>
+          </p>
+        </div>
+        <div className="flexcontainer-5">
+          <p className="text-3">
+            <span className="text-wrapper-13">
+              We hope this platform enables you to have a smoother experience during your studies with us.
+              <br />
+            </span>
+          </p>
+          <p className="text-3">
+            <span className="text-wrapper-13">Happy Coding!</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
